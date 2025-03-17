2d0bab2d752d657c7e9fb207ee0689540fee5481
2304419 <148556050+CardwellF@users.noreply.github.com>
2025-03-17 15:38:38 +0000

    Update Task 2B.py

--- Task 2B.py
+++ Task 2B.py
@@ -1,18 +1,1 @@
-#parrametric function
-import numpy as np
-import matplotlib.pyplot as plt
 
-# set range (t)
-t = np.linspace(0, 2*np.pi)
-# x equation
-x_t = 2*np.sin(t)*(np.exp(np.cos(t)))
-# y equation
-y_t = -(3/2)*np.cos(t)*(np.exp(np.sin(2*t)))
-# plot graph with 200 points
-plt.plot(x_t, y_t, bins=200, label="parametric functions")
-# show grid
-plt.grid()
-# create legend
-plt.legend()
-# show graph
-plt.show()
